60edd59460d521fb35af31259860058b228f9235
Nick Holden <nholden212@gmail.com>
2020-03-14 12:50:58 -0700

    Set up static route configuration and MVC framework

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
+const path = require('path');
+const viewsFolder = path.join(__dirname, "../bar-buddy-project/src", "views");
+const routeConfig = require('./src/config/route-config.js');
 
 app.use(bodyParser.json());
 app.use(
@@ -10,11 +13,10 @@ app.use(
   })
 );
 
+app.set('views', viewsFolder);
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('main.ejs');
-});
+routeConfig.init(app);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
